62ece325daeb82caf18aacf06a7a08b537d563e6
Arhaan25 <101710365+Arhaan25@users.noreply.github.com>
2023-01-02 17:42:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function draw() {
+    background('#05ede5');
+    fill('#11bf20');
+    stroke('#801c67');
+    square(noseX, noseY, difference);
+    document.getElementById("square_sides").innerHTML = "Width and Height of the square will be - " + difference + "px";
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function preload() {
+
+}
